b5e41c02f384d3cafe70eaa4a1ee56a1cab7f5bf
leedoopal <1233wsh@gmail.com>
2021-04-21 22:17:03 +0900

    add 3.6 putchar()

---
+++ chapter_03/3.6_putchar() 함수/3-6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  char var = 'A';
+  putchar(var);     // 함수의 괄호 안에 문자형 변수가 사용됨
+  putchar(var + 1); // 수식이 사용됨
+  putchar('\n');    // escape 문자가 사용됨
+  putchar('K');     // 문자형 상수가 사용됨
+  putchar('K' + 2); // 수식이 사용됨
+  putchar('\007');  // escape 문자가 사용됨(\007은 \a와 같은 의미)
+}
