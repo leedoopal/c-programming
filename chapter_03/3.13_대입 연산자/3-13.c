327398b9fe9110b3a6eb77c578dbdb093bc8efc1
leedoopal <1233wsh@gmail.com>
2021-04-21 23:08:33 +0900

    add 3.13 대입 연산자, 3.14 조건 연산자

---
+++ chapter_03/3.13_대입 연산자/3-13.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+  int a = 10, b = 3, c = 1;
+  a *= (b - 1); // a = a * (b - 1)의 의미
+  b /= 2 + 3;   // b = b / (2 + 3)이고 정수형끼리의 나눗셈이므로 결과는 0
+  c += 2;       // c = c + 2
+  printf("a = %d, b = %d, c = %d", a, b, c);
+}
